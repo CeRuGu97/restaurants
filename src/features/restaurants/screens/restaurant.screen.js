89dc2ad1376f231e1a5d105b67fff00d905c4cd0
CeRuGu97 <cesarchivas72@gmail.com>
2021-09-22 17:10:09 -0500

    despues de pasar jajaj

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -2,35 +2,52 @@ import React from 'react';
 import {
   SafeAreaView,
   View,
-  StyleSheet,
+  //StyleSheet,
   //Platform,
   StatusBar,
 } from 'react-native';
 import {Searchbar} from 'react-native-paper';
 import RestaurantInfoCard from '../components/restaurant-info-card.components';
+import styled from 'styled-components/native';
 
 //const isAndroid = Platform.OS === 'android';
-console.log(StatusBar.currentHeight);
+//console.log(StatusBar.currentHeight);
+
+const VistaSafArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight} && 'margin-top: ${StatusBar.currentHeight}px';
+`;
+
+const Search = styled(View)`
+  padding: 16px;
+  background-color: green;
+`;
+
+const List = styled(View)`
+  flex: 1;
+  padding: 16px;
+  background-color: blue;
+`;
 
 const RestaurantScreen = () => {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
+      <VistaSafArea>
+        <Search>
           <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
+        </Search>
+        <List>
           <RestaurantInfoCard />
-        </View>
-      </SafeAreaView>
+        </List>
+      </VistaSafArea>
     </>
   );
 };
-
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //marginTop: isAndroid ? 0 : StatusBar.currentHeight,
+    //marginTop: isAndroid StatusBar.currentHeight,
   },
   search: {
     padding: 16,
@@ -42,5 +59,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
   },
 });
+*/
 
 export default RestaurantScreen;
